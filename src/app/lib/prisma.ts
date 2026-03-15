ce6c7f68fdee68c87dc4ff8c963691fee9cfb2e9
Kyle <djmwinter@gmail.com>
2026-03-15 21:27:45 +0000

    refactor: migrate to PrismaPg adapter, update schema field names, and integrate `pg` dependencies

--- src/app/lib/prisma.ts
+++ src/app/lib/prisma.ts
@@ -1,11 +1,7 @@
-import { PrismaClient } from '@prisma/client'
-
-const globalForPrisma = global as unknown as { prisma: PrismaClient }
-
-export const prisma =
-    globalForPrisma.prisma ||
-    new PrismaClient({
-        //log: [ 'query' ],
-    })
-
-if (process.env.APP_ENV !== 'production') globalForPrisma.prisma = prisma
+import "dotenv/config";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "@prisma/client";
+const connectionString = `${process.env.DATABASE_URL}`;
+const adapter = new PrismaPg({ connectionString });
+const prisma = new PrismaClient({ adapter });
+export { prisma };
